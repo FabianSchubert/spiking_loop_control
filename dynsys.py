1a930698b0571893bb2247faa168ca138aec7bfa
FabianSchubert <fj.schubert@freenet.de>
2023-02-24 10:40:13 +0000

    add z Input to lif neuron

--- dynsys.py
+++ dynsys.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
 from pygenn.genn_model import (create_custom_neuron_class,
-							   create_dpf_class,
-							   create_custom_custom_update_class)
+                               create_dpf_class,
+                               create_custom_custom_update_class)
 
 import numpy as np
 
 def damped_spring_mass(x, u, k, g):
-	return np.array([x[1],
-					-x[0]*k -x[1]*g + u[1]])
+    return np.array([x[1],
+                    -x[0]*k -x[1]*g + u[1]])
--- synapses.py
+++ synapses.py
@@ -3,12 +3,12 @@
 from pygenn.genn_model import create_custom_weight_update_class
 
 fast_syn = {
-	"matrix_type": "DENSE_INDIVIDUALG",
-	"delay_steps": 0,
-	"w_update_model": "StaticPulse",
-	"wu_param_space": {},
-	#"wu_var_space": set when adding a synapse population
-	"wu_pre_var_space": {},
+    "matrix_type": "DENSE_INDIVIDUALG",
+    "delay_steps": 0,
+    "w_update_model": "StaticPulse",
+    "wu_param_space": {},
+    #"wu_var_space": set when adding a synapse population
+    "wu_pre_var_space": {},
     "wu_post_var_space": {},
     "postsyn_model": "DeltaCurr",
     "ps_param_space": {},
@@ -16,12 +16,12 @@ fast_syn = {
 }
 
 slow_syn = {
-	"matrix_type": "DENSE_INDIVIDUALG",
-	"delay_steps": 0,
-	"w_update_model": "StaticPulse",
-	"wu_param_space": {},
-	#"wu_var_space": set when adding a synapse population
-	"wu_pre_var_space": {},
+    "matrix_type": "DENSE_INDIVIDUALG",
+    "delay_steps": 0,
+    "w_update_model": "StaticPulse",
+    "wu_param_space": {},
+    #"wu_var_space": set when adding a synapse population
+    "wu_pre_var_space": {},
     "wu_post_var_space": {},
     "postsyn_model": "ExpCurr",
     "ps_param_space": {"tau": 0.1},
@@ -29,39 +29,39 @@ slow_syn = {
 }
 
 continuous_external_syn = {
-	"matrix_type": "DENSE_INDIVIDUALG",
-	"delay_steps": 0,
-	"w_update_model": create_custom_weight_update_class(
-		class_name="wu_mod_observe",
-		var_name_types=[("g", "scalar")],
-		param_names=[],
-		synapse_dynamics_code="$(addToInSyn, $(g) * $(x_pre));"
-	),
-	"wu_param_space": {},
-	#"wu_var_space": set when adding a synapse population
-	"wu_pre_var_space": {},
-	"wu_post_var_space": {},
-	"postsyn_model": "DeltaCurr",
-	"ps_param_space": {},
-	"ps_var_space": {}
+    "matrix_type": "DENSE_INDIVIDUALG",
+    "delay_steps": 0,
+    "w_update_model": create_custom_weight_update_class(
+        class_name="wu_mod_observe",
+        var_name_types=[("g", "scalar")],
+        param_names=[],
+        synapse_dynamics_code="$(addToInSyn, $(g) * $(x_pre));"
+    ),
+    "wu_param_space": {},
+    #"wu_var_space": set when adding a synapse population
+    "wu_pre_var_space": {},
+    "wu_post_var_space": {},
+    "postsyn_model": "DeltaCurr",
+    "ps_param_space": {},
+    "ps_var_space": {}
 }
 
 noise_syn = {
-	"matrix_type": "DENSE_INDIVIDUALG",
-	"delay_steps": 0,
-	"w_update_model": create_custom_weight_update_class(
-		class_name="wu_mod_noise",
-		var_name_types=[("g", "scalar")],
-		param_names=[],
-		synapse_dynamics_code="$(addToInSyn, $(g) * $(psi_pre));"
-	),
-	"wu_param_space": {},
-	#"wu_var_space": set when adding a synapse population
-	"wu_pre_var_space": {},
-	"wu_post_var_space": {},
-	"postsyn_model": "DeltaCurr",
-	"ps_param_space": {},
-	"ps_var_space": {}
+    "matrix_type": "DENSE_INDIVIDUALG",
+    "delay_steps": 0,
+    "w_update_model": create_custom_weight_update_class(
+        class_name="wu_mod_noise",
+        var_name_types=[("g", "scalar")],
+        param_names=[],
+        synapse_dynamics_code="$(addToInSyn, $(g) * $(psi_pre));"
+    ),
+    "wu_param_space": {},
+    #"wu_var_space": set when adding a synapse population
+    "wu_pre_var_space": {},
+    "wu_post_var_space": {},
+    "postsyn_model": "DeltaCurr",
+    "ps_param_space": {},
+    "ps_var_space": {}
 }
 
 
